cb619345e3a45f695345057da9b431c6a0fa3cb1
maks362880 <48396251+maks362880@users.noreply.github.com>
2022-10-05 22:26:25 +0300

    First release

---
+++ src/main/java/com/spring_first_program/customer/CustomerController.java
@@ -0,0 +1,80 @@
+package com.spring_first_program.customer;
+
+import org.apache.coyote.Request;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+@RequestMapping
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    // здесь будут методы обработки
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("index", "Главная страница");
+        return "main";
+    }
+
+
+    @GetMapping("/customers")
+    public String home(Model model) {
+        model.addAttribute("customers", customerService.listAll());
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String newCustomerForm(Map<String, Object> model) {
+        Customer customer = new Customer();
+        model.put("customer", customer);
+        return "new_customer";
+    }
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public String newCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customers";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.save(customer);
+        return "redirect:/customers";
+    }
+
+    @RequestMapping("/edit")
+    public ModelAndView editCustomerForm(@RequestParam long id) {
+        ModelAndView mav = new ModelAndView("edit_customer");
+        Customer customer = customerService.get(id);
+        mav.addObject("customer", customer);
+
+        return mav;
+    }
+
+    @RequestMapping("/delete")
+    public String deleteCustomerForm(@RequestParam long id) {
+        customerService.delete(id);
+        return "redirect:/customers";
+    }
+
+    @RequestMapping("/search")
+    public ModelAndView search(@RequestParam String keyword) {
+        List<Customer> result = customerService.search(keyword);
+        ModelAndView mav = new ModelAndView("search");
+        mav.addObject("result", result);
+
+        return mav;
+    }
+
+
+}
---
+++ src/test/java/com/spring_first_program/SpringFirstProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_first_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
